21fbe41c2469ea2f731a27a3afa2e2a7a7d55946
Lizette Romero <lmromero@anderson.edu>
2017-05-30 13:50:52 -0400

    add githiuband nasa API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, NavLink} from 'react-router-dom'
+import Github from './Github'
+import Nasa from './Nasa'
+import './app.css';
 
 class App extends Component {
   render() {
@@ -11,9 +13,21 @@ class App extends Component {
             <h3> Ain't no party like an</h3>
             <h1>API party</h1>
           </div>
+          <ul className="nav-link">
+            <li>
+              <NavLink to ={'/github'}>Github API</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/nasa'}>NASA API</NavLink>
+            </li>
+          </ul>
+          </div>
+          <Switch>
+            <Route path='/github' Component={Github} />
+            <Route path='/nasa' Component={Nasa} />
+            <Route render={() => <p>To get started, click one of the links above</p>} />
+          </Switch>
         </div>
-        
-      </div>
     );
   }
 }
